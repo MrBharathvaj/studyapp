62c85753caf66adf98d897ed943ae0c5c9529e34
Bharathvaj T <bharathvajt.it2020@gmail.com>
2023-09-11 16:14:46 +0530

    commit

--- app/portfolio/[category]/page.jsx
+++ app/portfolio/[category]/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from "./page.module.css"
 import Button from '@/components/Button/Button'
-import photo1 from "../../../public/category1.jpg"
+import photo1 from "../../../public/category2.jpeg"
 import Image from 'next/image'
 
 
@@ -20,6 +20,29 @@ const Category = ({ params }) => {
                     <Image src={photo1} fill={true} alt="picture" className={styles.img} />
                 </div>
             </div>
+
+            <div className={styles.item}>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.desciption}>description</p>
+                    <Button url="#" text="See More" />
+                </div>
+                <div className={styles.imgcontainer}>
+                    <Image src={photo1} fill={true} alt="picture" className={styles.img} />
+                </div>
+            </div>
+
+            <div className={styles.item}>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.desciption}>description</p>
+                    <Button url="#" text="See More" />
+                </div>
+                <div className={styles.imgcontainer}>
+                    <Image src={photo1} fill={true} alt="picture" className={styles.img} />
+                </div>
+            </div>
+
         </div>
     )
 }
